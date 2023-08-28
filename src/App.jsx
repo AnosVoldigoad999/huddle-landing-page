83090377d470b37458aa96b4f358afb50ffca481
AnosVoldigoad999 <oyedejiuzaymah@gmail.com>
2023-08-28 12:25:04 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import {PiTwitterLogoThin, PiInstagramLogoThin, PiFacebookLogoThin} from 'react-icons/pi'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App(){
+  return<>
+  <div className='logo' />
+  <div className="container">
+  <div className='image' />
+  <div className='everything'>
+    <h1>Build the community your fans will love</h1>
+    <p>Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion.</p>
+    <button>Register</button>
+    <footer>
+    <PiFacebookLogoThin className='icon' />
+    <PiTwitterLogoThin className='icon' />
+    <PiInstagramLogoThin className='icon' />
+    </footer>
+  </div>
+  </div>
+  </>
 }
-
-export default App
