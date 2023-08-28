7b5c09a5283892b8868409822e384e7992cfa64d
AnosVoldigoad999 <oyedejiuzaymah@gmail.com>
2023-08-28 14:35:46 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,18 @@ import {PiTwitterLogoThin, PiInstagramLogoThin, PiFacebookLogoThin} from 'react-
 export default function App(){
   return<>
   <div className='logo' />
-  <div className="container">
+ <div className="container"> 
   <div className='image' />
   <div className='everything'>
     <h1>Build the community your fans will love</h1>
     <p>Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion.</p>
     <button>Register</button>
-    <footer>
+  </div>
+  </div> 
+  <footer>
     <PiFacebookLogoThin className='icon' />
     <PiTwitterLogoThin className='icon' />
     <PiInstagramLogoThin className='icon' />
     </footer>
-  </div>
-  </div>
   </>
 }
